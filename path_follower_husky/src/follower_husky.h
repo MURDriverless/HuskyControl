085e05dc88bef948b1269186f817f9bcd4bb66dd
aldre1 <75785603+aldre1@users.noreply.github.com>
2021-07-26 14:02:21 +1000

    husky works

--- path_follower_husky/src/follower_husky.h
+++ path_follower_husky/src/follower_husky.h
@@ -14,6 +14,7 @@
 #include <vector>
 #include "path_point.h"
 #include <nav_msgs/Path.h>
+#include "spline.h"
 
 
 #define PI 3.14159265359
@@ -22,8 +23,10 @@
 #define MAX_ACC 11.772 //1.2*G
 #define MAX_DECEL -17.658 //-1.8*Gg
 #define MAX_STEER 0.8 //
-#define STEPSIZE 0.2 //spline step size   /////
+#define STEPSIZE 0.1 //spline step size   /////
+#define SPLINE_N 10
 #define DT 0.05
+#define STOP_INDEX 3 //centre point where the car should stop
 //PID gains:
 #define KP 1   /////
 #define KI 1   /////
@@ -31,13 +34,13 @@
 //pure pursuit gains
 #define K 0.1
 #define LFV  0.1  ///// look forward gain
-#define LFC  3.5    ///// look ahead distance
+#define LFC  3    ///// look ahead distance
 #define V_CONST 1.0 //constant velocity 1m/s (for now)
-#define MAX_V 0.75 //1.0
+#define MAX_V 0.7 //1.0
 #define MAX_W 30
 #define ERRL 0.5
 #define ERRA 0.01
-#define HZ 20 //from Dennis path follower
+#define HZ 10 //from Dennis path follower
 
 #define ODOM_TOPIC "/odometry/filtered" //"/mur/slam/Odom"
 #define CMDVEL_TOPIC "/husky_velocity_controller/cmd_vel"
@@ -48,12 +51,13 @@
 // #define STEER_TOPIC "/mur/control_desired"
 
 
-
+const bool DEBUG = true;
 class HuskyFollower
 {
 public:
     HuskyFollower(ros::NodeHandle n, double max_v, double max_w, double KP_dist, double KP_angle);
     void spin();
+    bool slowLapFinish = false;
 
 private:
 
@@ -70,6 +74,7 @@ private:
     float max_w;
     float KP_dist;
     float KP_angle;
+    float Lf = LFC;
 
     float car_x;
     float car_y;
@@ -78,19 +83,23 @@ private:
     float car_yaw;
     bool odom_msg_received = false;
     bool new_centre_points = false;
-    // float rearX;
-    // float rearY;
 
     std::vector<float> path_x;
     std::vector<float> path_y;
     bool path_msg_received = false;
     bool newGP = false;
-    
 
+    bool endOfPath = false;
+    bool endOfLap = false;
+    
+    
+    PathPoint currentGoalPoint = PathPoint(0,0);
     std::vector<PathPoint> centre_points; //centre line points of race tack
     std::vector<PathPoint> centre_splined;
-    int index;//centre splined index
+    std::vector<PathPoint> centre_endOfLap;
+    int index = -1;//centre splined index
     int oldIndex = -1;
+    int index_endOfLap = 1/STEPSIZE;
     std::vector<float> xp;
     std::vector<float> yp;
     std::vector<float> T;
@@ -108,11 +117,12 @@ private:
 
     //void accelerationControl();
     void steeringControl();
+    void generateSplines();
     //void updateRearPos();
     float getDistFromCar(PathPoint);
     float getAngleFromCar(PathPoint);
     void clearVars();
-    PathPoint getGoalPoint();
+    void getGoalPoint();
     void pushPathViz();
     float getSign(float&);
 };
